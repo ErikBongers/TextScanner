896d49189fbcdb3ba4bf7a5dfe871775ba56ae20
Erik Bongers <mail@erikbongers.net>
2025-12-29 16:49:59 +0100

    typo.

--- deploy_to_mopidy.sh
+++ deploy_to_mopidy.sh
@@ -6,4 +6,4 @@ cd ./text_scanner_py || exit
 
 maturin build
 
-cp ../target/maturin/libtext_scaner_py.so ~/programming/mopidy/mopidy_eboback/mopidy_eboback/lib/text_scaner_py.so
+cp ../target/maturin/libtext_scanner_py.so ~/programming/mopidy/mopidy_eboback/mopidy_eboback/lib/text_scanner_py.so
